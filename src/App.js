8e250f5fa6cf33861c8e29380f20e2789005f4b4
ana-vela <signing in to null>
2020-05-31 20:40:34 -0600

    Adding header and two tranlate boxes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div id="logo">Logo</div>
       </header>
+      <div className="container">
+        <div id="item">translate one</div>
+        <div id="item">translate two</div>
+      </div>
     </div>
   );
 }
