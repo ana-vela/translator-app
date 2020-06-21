1183ae1d9d2cb0f659977e225b6d07629b7d90a1
ana-vela <signing in to null>
2020-06-21 09:12:03 -0600

    Adding console log to test api

--- src/SourceLang.js
+++ src/SourceLang.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 function SourceLang() {
+  fetch(
+    "https://www.dictionaryapi.com/api/v3/references/spanish/json/language?key=xxx"
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => {
+      console.log(err);
+    });
   return <div>New Component</div>;
 }
 
