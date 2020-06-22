b62a1740e981951d101a33a7ca31209978e127db
ana-vela <signing in to null>
2020-06-22 14:18:42 -0600

    Console log nested array

--- src/SourceLang.js
+++ src/SourceLang.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function SourceLang() {
+  // const [items, setItems] =React.useState([]);
   fetch(
     "https://www.dictionaryapi.com/api/v3/references/spanish/json/language?key=xxx"
   )
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => console.log(data[0].meta.src))
     .catch((err) => {
       console.log(err);
     });
