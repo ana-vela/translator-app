6fc9f79acdafd404b1cd086d5a20bc19b8d0c5a5
ana-vela <signing in to null>
2020-06-30 20:42:53 -0600

    Adding Lexicala API

--- src/SourceLang.js
+++ src/SourceLang.js
@@ -1,16 +1,46 @@
 import React from "react";
+import { encode } from "base-64";
+
+let headers = new Headers();
+
+//headers.append('Content-Type', 'text/json');
+headers.append('Authorization', 'Basic ' + encode(process.env.REACT_APP_API_USER + ":" + process.env.REACT_APP_API_PW));
 
 function SourceLang() {
-  // const [items, setItems] =React.useState([]);
-  fetch(
-    "https://www.dictionaryapi.com/api/v3/references/spanish/json/language?key=xxx"
-  )
+  // const [items, setItems] = React.useState([]);
+  //
+  // React.useEffect(() => {
+  //   // async function getLang() {
+  //     const response = await fetch(url, {method: 'GET',
+  //   headers: headers});
+    // const body = await response.json();
+    // setItems(body.results && body.results.map(({ language_names }) => ({ label: language_names, value: language_names })));
+//   }
+//   getLang();
+// }, []);
+// }
+    fetch('https://dictapi.lexicala.com/languages', {method:'GET',
+            headers: headers
+           })
     .then((response) => response.json())
-    .then((data) => console.log(data[0].meta.src))
+    .then((data) => console.log(data.resources.global.source_languages, data))
     .catch((err) => {
       console.log(err);
     });
-  return <div>New Component</div>;
+  return (
+
+    // <div>
+    <h1>hi</h1>
+    //
+    // // <select>
+    // // {items.map(({ label, value }) => (
+    // //   <option key={value} value={value}>
+    // //   {label}
+    // //   </option>
+    // // ))}
+    // // </select>
+    // </div>
+  );
 }
 
 export default SourceLang;
