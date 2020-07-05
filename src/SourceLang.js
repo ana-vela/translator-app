e3704fc35c804144d897b1e008cdc7801b434559
ana-vela <signing in to null>
2020-07-05 16:14:31 -0600

    Accessing language names array

--- src/SourceLang.js
+++ src/SourceLang.js
@@ -4,42 +4,39 @@ import { encode } from "base-64";
 let headers = new Headers();
 
 //headers.append('Content-Type', 'text/json');
-headers.append('Authorization', 'Basic ' + encode(process.env.REACT_APP_API_USER + ":" + process.env.REACT_APP_API_PW));
+headers.append('Authorization', 'Basic ' + encode(process.env.REACT_APP_API_USERNAME + ":" + process.env.REACT_APP_API_PASSWORD));
 
 function SourceLang() {
-  // const [items, setItems] = React.useState([]);
-  //
-  // React.useEffect(() => {
-  //   // async function getLang() {
-  //     const response = await fetch(url, {method: 'GET',
-  //   headers: headers});
-    // const body = await response.json();
-    // setItems(body.results && body.results.map(({ language_names }) => ({ label: language_names, value: language_names })));
-//   }
-//   getLang();
-// }, []);
-// }
-    fetch('https://dictapi.lexicala.com/languages', {method:'GET',
-            headers: headers
-           })
-    .then((response) => response.json())
-    .then((data) => console.log(data.resources.global.source_languages, data))
-    .catch((err) => {
-      console.log(err);
-    });
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    async function getLang() {
+      const response = await fetch('https://dictapi.lexicala.com/languages', {method: 'GET',
+    headers: headers});
+    const body = await response.json();
+    setItems(Object.keys(body.language_names).map(language_names  => ({ label: language_names, value: language_names })));
+  }
+  getLang();
+}, []);
+
+    // fetch('https://dictapi.lexicala.com/languages', {method:'GET',
+    //         headers: headers
+    //        })
+    // .then((response) => response.json())
+    // .then((data) => console.log(data.resources.global.source_languages, data))
+    // .catch((err) => {
+    //   console.log(err);
+    // });
   return (
 
-    // <div>
-    <h1>hi</h1>
-    //
-    // // <select>
-    // // {items.map(({ label, value }) => (
-    // //   <option key={value} value={value}>
-    // //   {label}
-    // //   </option>
-    // // ))}
-    // // </select>
-    // </div>
+    <select>
+    {items.map(({ label, value }) => (
+      <option key={value} value={value}>
+      {label}
+      </option>
+    ))}
+    </select>
+
   );
 }
 
