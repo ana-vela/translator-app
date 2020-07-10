fe4b2e00bde9377d50c04ecabc1461a87481c076
ana-vela <signing in to null>
2020-07-09 21:14:08 -0600

    Testing filtering method by comparing country codes in language_names and source_languages arrays

--- src/SourceLang.js
+++ src/SourceLang.js
@@ -4,39 +4,58 @@ import { encode } from "base-64";
 let headers = new Headers();
 
 //headers.append('Content-Type', 'text/json');
-headers.append('Authorization', 'Basic ' + encode(process.env.REACT_APP_API_USERNAME + ":" + process.env.REACT_APP_API_PASSWORD));
+headers.append(
+  "Authorization",
+  "Basic " +
+    encode(
+      process.env.REACT_APP_API_USERNAME +
+        ":" +
+        process.env.REACT_APP_API_PASSWORD
+    )
+);
 
 function SourceLang() {
   const [items, setItems] = React.useState([]);
 
   React.useEffect(() => {
     async function getLang() {
-      const response = await fetch('https://dictapi.lexicala.com/languages', {method: 'GET',
-    headers: headers});
-    const body = await response.json();
-    setItems(Object.keys(body.language_names).map(language_names  => ({ label: language_names, value: language_names })));
-  }
-  getLang();
-}, []);
+      const response = await fetch("https://dictapi.lexicala.com/languages", {
+        method: "GET",
+        headers: headers,
+      });
+      const body = await response.json();
+      setItems(
+        Object.keys(body.language_names).map((language_names) => ({
+          label: language_names,
+          value: language_names,
+        }))
+      );
 
-    // fetch('https://dictapi.lexicala.com/languages', {method:'GET',
-    //         headers: headers
-    //        })
-    // .then((response) => response.json())
-    // .then((data) => console.log(data.resources.global.source_languages, data))
-    // .catch((err) => {
-    //   console.log(err);
-    // });
+      //testing filter method
+      let comparing = Object.keys(body.language_names).filter((language) =>
+        body.resources.global.source_languages.includes(language)
+      );
+      console.log(comparing);
+    }
+    getLang();
+  }, []);
+
+  // fetch('https://dictapi.lexicala.com/languages', {method:'GET',
+  //         headers: headers
+  //        })
+  // .then((response) => response.json())
+  // .then((data) => console.log(data.resources.global.source_languages, data))
+  // .catch((err) => {
+  //   console.log(err);
+  // });
   return (
-
     <select>
-    {items.map(({ label, value }) => (
-      <option key={value} value={value}>
-      {label}
-      </option>
-    ))}
+      {items.map(({ label, value }) => (
+        <option key={value} value={value}>
+          {label}
+        </option>
+      ))}
     </select>
-
   );
 }
 
