21f134176b33c2ff0c9e8548aee3cc8204199cd9
ana-vela <signing in to null>
2020-07-14 19:05:01 -0600

    Adding source language names to dropdown

--- src/SourceLang.js
+++ src/SourceLang.js
@@ -24,41 +24,27 @@ function SourceLang() {
         headers: headers,
       });
       const body = await response.json();
-      setItems(
-        Object.keys(body.language_names).map((language_names) => ({
-          label: language_names,
-          value: language_names,
-        }))
-      );
-
-      //testing filter method
-      // let comparing = Object.keys(body.language_names).filter((language) =>
-      //   body.resources.global.source_languages.includes(language)
-      // );
-      // console.log(comparing);
-
-      // testing reduce method
       const sourceLang = body.resources.global.source_languages;
       const langName = body.language_names;
+
       function compare(sourceLang, langName) {
         return sourceLang.reduce(function (newObj, key) {
           if (key in langName) newObj[key] = langName[key];
           return newObj;
         }, {});
       }
-      console.log(compare(sourceLang, langName));
+      const sourceLangNames = compare(sourceLang, langName);
+
+      setItems(
+        Object.values(sourceLangNames).map((sourceLangName) => ({
+          label: sourceLangName,
+          value: sourceLangName,
+        }))
+      );
     }
     getLang();
   }, []);
 
-  // fetch('https://dictapi.lexicala.com/languages', {method:'GET',
-  //         headers: headers
-  //        })
-  // .then((response) => response.json())
-  // .then((data) => console.log(data.resources.global.source_languages, data))
-  // .catch((err) => {
-  //   console.log(err);
-  // });
   return (
     <select>
       {items.map(({ label, value }) => (
