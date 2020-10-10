24bfba944e46bde63e0636eee867a33d31a79d59
Hunter Callaway <HunterCallaway@hotmail.com>
2020-10-09 19:39:07 -0500

    Using a new URL for the fetch statement.

--- src/SearchButton.js
+++ src/SearchButton.js
@@ -14,23 +14,27 @@ const SearchButton = () => {
     //rather than the searchTerm, which is tied to an event listener that listens for every keystroke.
     const [search, setSearch] = useState('');
 
-    const [entryId, setEntryId] = useState();
+    //const [firstEntryId, setFirstEntryId] = useState();
 
     //This variable will set the ref object's `.current` property to false initially.
     const isMounted = useRef(false);
+
+    const [translationEntry, setTranslationEntry] = useState();
 
     /*
       This use effect will run when the button is clicked.
       Due to the conditional block, the effect will not run when the component is initially mounted.
         The `else` block will update the value of the `.current` property to `true`
         and allow the effect to run after the component has been mounted and the button has been clicked.
-      It first makes a call to the API to obtain the `entryId` for the search value.
+      
     */
+
+
     useEffect(() => {
 
         if (isMounted.current) {
-            async function getEntryId() {
-                const firstRequest = await fetch(`https://cors-anywhere.herokuapp.com/https://api.collinsdictionary.com/api/v1/dictionaries/${dictionaryValue}/search/?q=${search}`, {
+            const getTranslation = async () => {
+                const firstRequest = await fetch(`https://cors-anywhere.herokuapp.com/https://api.collinsdictionary.com/api/v1/dictionaries/${dictionaryValue}/search/first/?q=${search}`, {
                     method: 'GET', 
                     headers: {
                         'accessKey': process.env.REACT_APP_COLLINS_API_KEY,
@@ -39,30 +43,36 @@ const SearchButton = () => {
                 const body = await firstRequest.json();
                 console.log(body);
 
-                if (body.length > 0 ) {
-                    setEntryId(body.find(item => item[0]));
-                }
+
+        
+                //if (body.length > 0 ) {
+                    //setFirstEntryId(body.results.filter((item) => item.entryId === search));
+                //}
                 
-                const secondRequest = await fetch(`https://cors-anywhere.herokuapp.com/https://api.collinsdictionary.com/api/v1/dictionaries/${dictionaryValue}/entries/${entryId}`, {
-                    method: 'GET', 
-                    headers: {
-                        'accessKey': process.env.REACT_APP_COLLINS_API_KEY,
-                    }
-                })
-                const translationObject = await secondRequest.json();
-                console.log(translationObject);
+                // const secondRequest = await fetch(`https://cors-anywhere.herokuapp.com/https://api.collinsdictionary.com/api/v1/dictionaries/${dictionaryValue}/entries/${body.results[0].entryId}?format=html`, {
+                //     method: 'GET', 
+                //     headers: {
+                //         'accessKey': process.env.REACT_APP_COLLINS_API_KEY,
+                //     }
+                // })
+                // const translationObject = await secondRequest.json();
+                // console.log(translationObject);
             }
-            getEntryId();
+
+            getTranslation();
+            
         } else {
-            isMounted.current = true;
+           isMounted.current = true;
         }
+
+      
         
 
         //async function getTranslation() {
 
         //}
        
-    }, [dictionaryValue, search, entryId]);
+    }, [dictionaryValue, search]); // eslint-disable-line react-hooks/exhaustive-deps
 
     //This function prevents the page from refreshing when the button is clicked
     //and runs the search state update function.
